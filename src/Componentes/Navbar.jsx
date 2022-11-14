896dea38d83a37ad47e1574eb491072afacc33a0
annie <fav.hala@gmail.com>
2022-11-14 19:38:51 -0300

    test

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-        <h1 className="text-3xl mt-20 mr-20 ml-20 text-center font-bold text-pink-800">"The importance of physical contact in babies and techniques for the care of the body of teachers"</h1>
+        <div className="inline-flex">
+        <img className="w-40" src="https://s1.significados.com/foto/ramo-de-flor-de-cerejeira-57615baf1fcaf-600x600_sm.jpg"/>
+        <h1 className="text-3xl mt-20 mr-20 ml-10 text-center font-bold text-pink-800">"The importance of physical contact in babies and techniques for the care of the body of teachers"</h1>
+        </div>
         <div className="content-center">
-        <img className="rounded-full mt-10 w-4/12 p-4 mx-auto flex justify-center" src="https://www.dans-ma-tribu.fr/wp-content/uploads/2015/09/bebe-main-maman.jpg" alt=""/>
+        <img className="rounded-full mt-4 w-4/12 p-4 mx-auto flex justify-center" src="https://www.dans-ma-tribu.fr/wp-content/uploads/2015/09/bebe-main-maman.jpg" alt=""/>
         </div>      
         </>
     )
