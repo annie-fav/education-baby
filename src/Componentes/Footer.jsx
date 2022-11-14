39cf2cc65cf04f86582d5925dd4858ffe1e870a1
annie <fav.hala@gmail.com>
2022-11-14 19:08:31 -0300

    education-baby

---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+         <div className=" italic font-serif mt-4 ml-12 mr-12 text-justify indent-6">
+          <h3 className="text-xl font-bold text-pink-800">Bibliography</h3>
+          <br></br>
+          <ul className="text-left ml-8">
+            <li>* “Gestar y nacer desde el cuerpo, instructorado de yoga, módulo I” por Vianca Diz Perez. . *</li>
+            <li>* Pranayama.</li>
+            <li>* Concentration.</li>
+            <li>* Physical contact has an effect on the baby's brain.</li>
+            <li>* Physical contact strngthens the baby's bond.</li>
+          </ul>
+          <hr className="my-8 h-1 h-px bg-rose-300 h-px border-0"></hr>
+          <p className="text-right italic mr-20 font-bold">Florencia Anabel Vazquez</p>
+          </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Componentes/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+        <h1 className="text-3xl mt-20 mr-20 ml-20 text-center font-bold text-pink-800">"The importance of physical contact in babies and techniques for the care of the body of teachers"</h1>
+        <div className="content-center">
+        <img className="rounded-full mt-10 w-4/12 p-4 mx-auto flex justify-center" src="https://www.dans-ma-tribu.fr/wp-content/uploads/2015/09/bebe-main-maman.jpg" alt=""/>
+        </div>      
+        </>
+    )
+}
+
+export default Navbar
